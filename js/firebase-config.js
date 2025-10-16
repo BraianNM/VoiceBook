1a7ab29feff541a75eeae581f71db8731016772c
BraianNM <90859852+BraianNM@users.noreply.github.com>
2025-10-15 22:04:08 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,11 @@
 // 🔥 CONFIGURACIÓN DE FIREBASE - REEMPLAZA CON TUS DATOS REALES
 const firebaseConfig = {
-    apiKey: "TU_API_KEY",
-    authDomain: "TU_PROYECTO.firebaseapp.com",
-    projectId: "TU_PROYECTO",
-    storageBucket: "TU_PROYECTO.appspot.com",
-    messagingSenderId: "TU_SENDER_ID",
-    appId: "TU_APP_ID"
+    apiKey: "AIzaSyC6G6NgMqrMDyd5PB6_HmLNHpPU-vNJdf0",
+    authDomain: "voicebook-8ba6c.firebaseapp.com",
+    projectId: "voicebook-8ba6c",
+    storageBucket: "voicebook-8ba6c.firebasestorage.app",
+    messagingSenderId: "534166349589",
+    appId: "1:534166349589:web:e5e9c11b488fa52828ab1c"
 };
 
 // Inicializar Firebase
